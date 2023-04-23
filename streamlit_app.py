2dacf64b17f405a656a6b1ebff28f39029b97a04
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-04-23 10:56:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
-import plotly.figure_factory as ff
+import plotly.plotly as py
+import plotly.figure_factory as FF
 
 # Add histogram data
 x1 = np.random.randn(200) - 2
@@ -13,7 +14,7 @@ hist_data = [x1, x2, x3]
 group_labels = ['Group 1', 'Group 2', 'Group 3']
 
 # Create distplot with custom bin_size
-fig = ff.create_distplot(
+fig = FF.create_distplot(
         hist_data, group_labels, bin_size=[.1, .25, .5])
 
 # Plot!
