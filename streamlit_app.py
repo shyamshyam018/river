4a53354ec68093b1420f710952e8687baaf32b1e
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-04-23 10:58:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,3 @@
 import streamlit as st
-import numpy as np
-import plotly.plotly as py
-import plotly.figure_factory as FF
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = FF.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+st.write("VANAKKAM DA MAPLE")
