59134b848feac5a60b1d00fbe1109643e9860b24
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-02 22:03:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.title("HI THIS IS SHYAM")
+st.title("HI THIS MACHINE LEARNING MODEL IS CREATED BY SHYAM RAJ")
 with st.sidebar:
     
-    selected = option_menu('HI THIS MACHINE LEARNING MODEL IS CREATED BY SHYAM RAJ ',
+    selected = option_menu('CHOOSE THE PREDICTION MODEL',
                           
-                          ['WHO AM I ',
+                          [':moon',
                            'WHY ARE YOU HERE',
                            'CONTACT ?'],
                           icons=['activity','heart','person'],
