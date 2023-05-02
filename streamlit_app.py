48c0f668cda98bf96d9309080e456423da382843
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-02 22:17:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ if selected == 'WHAT CAN THIS MODEL DETECT':
     
 elif selected == 'UPLOAD YOUR IMAGE':
     uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
