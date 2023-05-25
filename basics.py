275e06ccd0aa329db2952d87cb6c33df9fde4dcd
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-25 21:00:06 +0530

    Update and rename streamlit_app.py to basics.py

---
+++ basics.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("THIS_IS_MY_FIRST_STREAMLIT_APP")
+st.markdown("""
+:world HOW ARE YOU DARLING
+:moon I THINK YOU ARE DOING FINE
+:tree OKAY BYE BYE TAKE CARE
+""")
--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("THIS_IS_MY_FIRST_STREAMLIT_APP")
-st.header("so what")
-st.subheader("okay i understand")
-st.markdown("""
-okay now this is similar to a multi t=line comment
-"""
-           )
